12b435468fb6d34d52e1aa49b04de62f24d87cea
sukoodima-code <sukoodima@gmail.com>
2025-09-14 15:04:19 +0500

    backend changes

--- backend/models/car.py
+++
@@ -1,8 +0,0 @@
-class Car:
-    def __init__(self, id, model, status):
-        self.id = id
-        self.model = model
-        self.status = status
-    
-    def to_dict(self):
-        return {"id": self.id, "model": self.model, "status": self.status}
